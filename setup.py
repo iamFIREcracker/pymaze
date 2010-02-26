08842b58d4152e3184891e32b2b4f9f1a9465e82
Matteo Landi <landimatte@gmail.com>
2010-02-27 00:23:49 +0100

    Add source directory and default install script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='pymaze',
+      version='0.0',
+      author='Matteo Landi',
+      description='Python library used to generate and solve mazes'
+      package_dir={'pymazelib': 'pymazelib'},
+      packages=['pymazelib']
+     )
