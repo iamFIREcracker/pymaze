e411bdcf677c2cfb8c85a06ca83b980d60e4c1cf
Matteo Landi <landimatte@gmail.com>
2010-02-28 04:17:33 +0100

    Add randomized prim's generating algorithm.
    Need some doc.

--- pymazelib/maze.py
+++ pymazelib/maze.py
@@ -10,8 +10,9 @@ from random import shuffle
 CELL = '0123456789abcdef'
 
 # generating algorithms
-(DEPTH_FIRST) = xrange(1)
-algorithms = {'depth-first': DEPTH_FIRST}
+(DEPTH_FIRST, RANDOMIZED_PRIM) = xrange(2)
+algorithms = {'depth-first': DEPTH_FIRST,
+              'randomized-prim': RANDOMIZED_PRIM,}
 
 def opposite_direction(dir):
   """Return the opposite of the given direction.
@@ -21,6 +22,17 @@ def opposite_direction(dir):
   elif dir == EAST: return WEST
   elif dir == SOUTH: return NORTH
   elif dir == WEST: return EAST
+
+def beyond_wall(i, j, dir, rows, columns):
+  """Return the indices of the cell beyond the wall located at the given
+  direction.
+
+  """
+  if dir == NORTH and i > 0: return (i - 1, j)
+  elif dir == EAST and j < columns - 1: return (i, j + 1)
+  elif dir == SOUTH and i < rows - 1: return (i + 1, j)
+  elif dir == WEST and j > 0: return (i, j - 1)
+  else: return (None, None)
 
 class Cell(object):
   def __init__(self):
@@ -104,10 +116,41 @@ class Maze(object):
           cell_stack.append((ni, nj))
           visited[(ni, nj)] = True
           self.modified = [(ci, cj), (ni, nj)]
+          yield True
           break
-      yield True
     yield False
 
+  def generate_randomized_prim(self):
+    """Generate the maze by using the randomized prim's algorithm.
+    XXX
+
+    """
+    visited = {}
+    (rows, columns) = self.size
+    (i, j) = (randint(0, rows - 1), randint(0, columns - 1))
+    visited[(i, j)] = True
+
+    wall_stack = [(NORTH, (i, j)), (EAST, (i, j)),
+                  (SOUTH, (i, j)), (WEST, (i, j))]
+    while wall_stack:
+      i = randint(0, len(wall_stack) - 1)
+      (dir, (ci, cj)) = wall_stack.pop(i) 
+      (ni, nj) = beyond_wall(ci, cj, dir, rows, columns)
+      if ni is not None and nj is not None and (ni, nj) not in visited:
+        self.grid[ci][cj].knock_down(dir)
+        self.grid[ni][nj].knock_down(opposite_direction(dir))
+        if self.grid[ni][nj].north_wall:
+          wall_stack.append((NORTH, (ni, nj)))
+        if self.grid[ni][nj].east_wall:
+          wall_stack.append((EAST, (ni, nj)))
+        if self.grid[ni][nj].south_wall:
+          wall_stack.append((SOUTH, (ni, nj)))
+        if self.grid[ni][nj].west_wall:
+          wall_stack.append((WEST, (ni, nj)))
+        visited[(ni, nj)] = True
+        self.modified = [(ci, cj), (ni, nj)]
+        yield True
+    yield False
 
   def generate(self):
     """Return a generator object to be used for the maze generation.
@@ -115,6 +158,8 @@ class Maze(object):
     """
     if self.algorithm == DEPTH_FIRST:
       return self.generate_depth_first()
+    elif self.algorithm == RANDOMIZED_PRIM:
+      return self.generate_randomized_prim()
 
   def __getitem__(self, coord):
     """Return the cell addressed by the given coordinates.
