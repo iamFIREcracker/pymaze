f47b833dd216da5824a25b771155dd2ace8a3f95
Matteo Landi <landimatte@gmail.com>
2010-03-02 20:35:24 +0100

    Add 'wall-follower' solver.

--- pymazelib/solvers.py
+++ pymazelib/solvers.py
@@ -4,6 +4,11 @@ from random import randint
 from random import shuffle
 
 from maze import Maze
+from maze import NORTH
+from maze import EAST
+from maze import SOUTH
+from maze import WEST
+from maze import DIRECTIONS
 from maze import opposite_direction
 
 """Dictionary used to map an algorithm to its generator function.
@@ -11,13 +16,14 @@ from maze import opposite_direction
 """
 solvers = {
            'Depth First': lambda m: depth_first(m),
+           'Wall Follower': lambda m: wall_follower(m),
           }
 
 def depth_first(maze):
-  """Pick up a a random cell and mark it as a visited. Then look for open
-  directions, choose one randomly, and add the current cell together with the
-  neighbor one to cell stack. Pop a cell from the stack and repeat the steps
-  untill the end cell is found.
+  """Pick up the start cell and then look for open directions, choose one
+  randomly, and add the current cell together with the neighbor one to a cell
+  stack. Then pop a cell from the stack and repeat the steps untill the end
+  cell is found.
 
   """
   cell_stack = [maze[(0, 0)]]
@@ -42,3 +48,33 @@ def depth_first(maze):
       maze.modified = [cell.coords]
     yield True
   yield False
+
+def wall_follower(maze):
+  """Each time we have a pool of possible directions, we start by analysing the
+  one of the left of current one.
+
+  """
+  cell_stack = [(maze[(0, 0)], EAST)]
+  while True:
+    (cell, curr_dir) = cell_stack.pop()
+    if cell.end:
+      break
+    curr_dir = (curr_dir - 1) # let's consider first our left
+    for dir in xrange(curr_dir, curr_dir + DIRECTIONS):
+      dir = (dir % DIRECTIONS)
+      if cell.directions[dir]:
+        neighbor = cell.neighbors[dir]
+        neighbor.active = True
+        cell.directions[dir] = False
+        neighbor.directions[opposite_direction(dir)] = False
+        cell_stack.append((cell, curr_dir))
+        cell_stack.append((neighbor, dir))
+        maze.modified = [neighbor.coords]
+        break
+    else:
+      cell.active = False
+      cell.backward = True
+      maze.modified = [cell.coords]
+    yield True
+  yield False
+
