acd11e8477dfda1c6a1c48eed11abb15a24a6270
Matteo Landi <landimatte@gmail.com>
2010-03-02 11:18:16 +0100

    Add support for the new API.

--- pymazelib/generators.py
+++ pymazelib/generators.py
@@ -5,7 +5,6 @@ from random import shuffle
 
 from maze import Maze
 from maze import opposite_direction
-from maze import beyond_wall
 
 """Dictionary used to map an algorithm to its generator function.
 
@@ -15,36 +14,6 @@ generators = {
               'Recursive Backtracker': lambda m: recursive_backtracker(m),
              }
 
-def recursive_backtracker(maze):
-  """Pick up a a random cell and mark it as a visited. Then look for neighbors
-  which have not been visited yet, knock down the walls in between and set the
-  new cell as the current one. Repeat these steps untill all the cells of the
-  grid have been visited.
-
-  """
-  visited = {}
-  (rows, columns) = maze.size
-  (i, j) = (randint(0, rows - 1), randint(0, columns - 1))
-  visited[(i, j)] = True
-
-  total = rows * columns
-  cell_stack = [(i, j)]
-  while len(visited) < total:
-    (ci, cj) = cell_stack.pop()
-    neighborhood = maze.neighbors(ci, cj)
-    shuffle(neighborhood)
-    for (dir, (ni, nj)) in neighborhood:
-      if (ni, nj) not in visited:
-        maze[(ci, cj)].knock_down(dir)
-        maze[(ni, nj)].knock_down(opposite_direction(dir))
-        cell_stack.append((ci, cj))
-        cell_stack.append((ni, nj))
-        visited[(ni, nj)] = True
-        maze.modified = [(ci, cj), (ni, nj)]
-        yield True
-        break
-  yield False
-
 def prim(maze):
   """Pick up a random cell, mark it as visited, and add its walls inside a
   list of walls. Extract a random wall from the list: if the cell on the
@@ -53,22 +22,55 @@ def prim(maze):
   the cells have been visited.
 
   """
-  visited = {}
   (rows, columns) = maze.size
   (i, j) = (randint(0, rows - 1), randint(0, columns - 1))
-  visited[(i, j)] = True
+  maze[(i, j)].carved = True
 
-  wall_stack = map(lambda d: (d, (i, j)), maze[(i, j)].walls)
+  wall_stack = [(maze[(i, j)], dir) for dir in maze[(i, j)].intact_walls()]
   while wall_stack:
     i = randint(0, len(wall_stack) - 1)
-    (dir, (ci, cj)) = wall_stack.pop(i) 
-    (ni, nj) = beyond_wall(ci, cj, dir, rows, columns)
-    if ni is not None and nj is not None and (ni, nj) not in visited:
-      maze[(ci, cj)].knock_down(dir)
-      maze[(ni, nj)].knock_down(opposite_direction(dir))
-      for dir in maze[(ni, nj)].walls:
-        wall_stack.append((dir, (ni, nj)))
-      visited[(ni, nj)] = True
-      maze.modified = [(ci, cj), (ni, nj)]
+    (cell, dir) = wall_stack.pop(i) 
+    neighbor = cell.neighbors[dir]
+    if neighbor and not neighbor.carved:
+      cell.knock_down(dir)
+      neighbor.knock_down(opposite_direction(dir))
+      neighbor.carved = True
+      wall_stack += \
+          [(neighbor, dir) for dir in neighbor.intact_walls()]
+      maze.modified = [cell.coords, neighbor.coords]
       yield True
+  yield False
+
+def recursive_backtracker(maze):
+  """Pick up a a random cell and mark it as a visited. Then look for neighbors
+  which have not been visited yet, knock down the walls in between and set the
+  new cell as the current one. Add the old cell and the new one to the cell
+  stack and repeat the steps by extracting a cell from the stack untill all the
+  cells of the grid have been visited.
+
+  """
+  (rows, columns) = maze.size
+  total = rows * columns
+
+  (i, j) = (randint(0, rows - 1), randint(0, columns - 1))
+  maze[(i, j)].carved = True
+  carved = 1
+  cell_stack = [maze[(i, j)]]
+  while carved < total:
+    cell = cell_stack.pop()
+    neighborhood = cell.neighbors
+    directions = range(len(neighborhood))
+    shuffle(directions)
+    for dir in directions:
+      neighbor = neighborhood[dir]
+      if neighbor and not neighbor.carved:
+        cell.knock_down(dir)
+        neighbor.knock_down(opposite_direction(dir))
+        neighbor.carved = True
+        carved += 1
+        cell_stack.append(cell)
+        cell_stack.append(neighbor)
+        maze.modified = [cell.coords, neighbor.coords]
+        yield True
+        break
   yield False
--- pymazelib/solvers.py
+++ pymazelib/solvers.py
@@ -4,16 +4,41 @@ from random import randint
 from random import shuffle
 
 from maze import Maze
-from maze import NORTH
 from maze import opposite_direction
-from maze import beyond_wall
 
 """Dictionary used to map an algorithm to its generator function.
 
 """
 solvers = {
-           'Always Turn Left': lambda m: always_turn_right(m),
+           'Depth First': lambda m: depth_first(m),
           }
 
-def always_turn_left(maze):
+def depth_first(maze):
+  """Pick up a a random cell and mark it as a visited. Then look for open
+  directions, choose one randomly, and add the current cell together with the
+  neighbor one to cell stack. Pop a cell from the stack and repeat the steps
+  untill the end cell is found.
+
+  """
+  cell_stack = [maze[(0, 0)]]
+  while True:
+    cell = cell_stack.pop()
+    if cell.end:
+      break
+    directions = cell.open_directions()
+    shuffle(directions)
+    for dir in directions:
+      neighbor = cell.neighbors[dir]
+      neighbor.active = True
+      cell.directions[dir] = False
+      neighbor.directions[opposite_direction(dir)] = False
+      cell_stack.append(cell)
+      cell_stack.append(neighbor)
+      maze.modified = [neighbor.coords]
+      break
+    else:
+      cell.active = False
+      cell.backward = True
+      maze.modified = [cell.coords]
+    yield True
   yield False
